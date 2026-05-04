2e56a3c0aa9734c32e3ac174ebfa0295cd94f680
MajCapelnik <maj.capelnik@gmail.com>
2026-05-04 16:14:38 +0200

    Update admin.py

--- djangoapi/parcels/admin.py
+++ djangoapi/parcels/admin.py
@@ -1,6 +1,13 @@
-from django.contrib import admin
 from django.contrib.gis import admin
 from .models import Parcels, Parcels_Owners
 
+class ParcelsAdmin(admin.GISModelAdmin):
+    gis_widget_kwargs = {
+        'attrs': {
+            'default_lon': 15.0806,
+            'default_lat': 46.5044,
+            'default_zoom': 14,
+        }
+    }
 admin.site.register(Parcels_Owners, admin.ModelAdmin)
-admin.site.register(Parcels, admin.GISModelAdmin)
+admin.site.register(Parcels, ParcelsAdmin)
