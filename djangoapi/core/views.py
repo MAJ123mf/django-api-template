246711d1377e83f5350e45490445984c8f8e888b
MajCapelnik <maj.capelnik@gmail.com>
2026-05-04 16:20:45 +0200

    Refactor views and clean up unused code

--- djangoapi/core/views.py
+++ djangoapi/core/views.py
@@ -10,12 +10,10 @@ from django.shortcuts import redirect
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.http import JsonResponse
 
-
 # CSRF žeton uturi v cooki
 # @ensure_csrf_cookie
 # def get_csrf_token(request):
 #     return JsonResponse({'detail': 'CSRF cookie set'})
-
 
 def custom_logout_view(request):
     logout(request)
@@ -23,15 +21,11 @@ def custom_logout_view(request):
     
 def notLoggedIn(request):
     next_url = request.GET.get('next', '/swagger/')   # če nisi prijavljen, te preusmeri na login stran
-    return redirect(f'/accounts/login/?next={next_url}')  
-
+    return redirect(f'/accounts/login/?next={next_url}')    
 
 class HelloWord(View):
     def get(self, request):
         return JsonResponse({"ok":True,"message": "Core. Hello world", "data":[]})
-
-
-
 
 class LoginView(View):
    def post(self, request, *args, **kwargs):
@@ -63,17 +57,12 @@ class LoginView(View):
            time.sleep(seconds)
            return JsonResponse({"ok":False,"message": "Wrong user or password", "data":[]})
 
-
-
-
 class LogoutView(LoginRequiredMixin, View):
     def post(self, request, *args, **kwargs):
         username=request.user.username
         logout(request) #removes from the header of the request
                             #the the session_id, stored in a cookie
         return JsonResponse({"ok":True,"message": "The user {0} is now logged out".format(username), "data":[]})
-
-
 
 class IsLoggedIn(View):
     def post(self, request, *args, **kwargs):
@@ -83,8 +72,6 @@ class IsLoggedIn(View):
             return JsonResponse({"ok":True,"message": "You are authenticated", "data":[{'username':request.user.username}]})
         else:
             return JsonResponse({"ok":False,"message": "You are not authenticated", "data":[]})
-
-
 
 class CheckLoginView(View):
     def get(self, request, *args, **kwargs):
@@ -104,47 +91,3 @@ class CheckLoginView(View):
                 "data": []
             })
 
-
-
-            
-# Login z Json in podporo tokenu Csrf
-#@method_decorator(ensure_csrf_cookie, name='dispatch')
-#class LoginView(View):
-#    def post(self, request, *args, **kwargs):
-#        if request.user.is_authenticated:
-#            username = request.user.username
-#            return JsonResponse({
-#                "ok": True,
-#                "message": f"The user {username} already is authenticated",
-#                "data": [{'username': username}]
-#            })
-#
-#        # ✅ Dodano: preberi JSON telo, če je prisotno
-#        if request.content_type == 'application/json':
-#            try:
-#                data = json.loads(request.body)
-#                username = data.get('username')
-#                password = data.get('password')
-#            except json.JSONDecodeError:
-#                return JsonResponse({"ok": False, "message": "Invalid JSON", "data": []}, status=400)
-#        else:
-#            username = request.POST.get('username')
-#            password = request.POST.get('password')
-#
-#        print('Logiranje uporabnika:', username, '-', password)
-#
-#        user = authenticate(username=username, password=password)
-#        if user:
-#            login(request, user)
-#            return JsonResponse({
-#                "ok": True,
-#                "message": f"User {username} logged in",
-#                "data": [{
-#                    "username": username,
-#                    "groups": list(user.groups.values_list("name", flat=True))
-#                }]
-#            })
-#        else:
-#            # Naključna zamuda za varnost
-#            time.sleep(random.uniform(0, 1))
-#            return JsonResponse({"ok": False, "message": "Wrong user or password", "data": []})        
