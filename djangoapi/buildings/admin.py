4a3b3ac24e40041dea947fd2f41ab0248649104e
MajCapelnik <maj.capelnik@gmail.com>
2026-05-04 16:13:54 +0200

    Refactor Buildings admin registration with custom admin class

--- djangoapi/buildings/admin.py
+++ djangoapi/buildings/admin.py
@@ -1,6 +1,12 @@
-from django.contrib import admin
 from django.contrib.gis import admin
-from .models import Buildings, Owners
+from .models import Buildings
 
-admin.site.register(Owners, admin.ModelAdmin)
-admin.site.register(Buildings, admin.GISModelAdmin)
+class BuildingsAdmin(admin.GISModelAdmin):
+    gis_widget_kwargs = {
+        'attrs': {
+            'default_lon': 15.0806,
+            'default_lat': 46.5044,
+            'default_zoom': 14,
+        }
+    }
+admin.site.register(Buildings, BuildingsAdmin)
