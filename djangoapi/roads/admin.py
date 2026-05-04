e56e6d4ee89a092d948b4f5b30f59e95184e6048
MajCapelnik <maj.capelnik@gmail.com>
2026-05-04 16:15:05 +0200

    Update admin.py

--- djangoapi/roads/admin.py
+++ djangoapi/roads/admin.py
@@ -1,5 +1,12 @@
-from django.contrib import admin
 from django.contrib.gis import admin
 from .models import Roads
 
-admin.site.register(Roads, admin.GISModelAdmin) 
+class RoadsAdmin(admin.GISModelAdmin):
+    gis_widget_kwargs = {
+        'attrs': {
+            'default_lon': 15.0806,
+            'default_lat': 46.5044,
+            'default_zoom': 14,
+        }
+    }
+admin.site.register(Roads, RoadsAdmin)
