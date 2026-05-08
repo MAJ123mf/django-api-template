0bf30ed43721dfe2b79701c0ab0a46ffdc7c76cf
MajCapelnik <maj.capelnik@gmail.com>
2026-05-08 15:47:20 +0200

    Update admin.py

--- djangoapi/parcels/admin.py
+++ djangoapi/parcels/admin.py
@@ -1,4 +1,7 @@
 from django.contrib.gis import admin
+from django.contrib import admin as django_admin  # ← dodaj
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from .models import Parcels, Parcels_Owners
 
 class ParcelsAdmin(admin.GISModelAdmin):
@@ -9,5 +12,16 @@ class ParcelsAdmin(admin.GISModelAdmin):
             'default_zoom': 14,
         }
     }
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_groups')
+    
+    def get_groups(self, obj):
+        return ', '.join([g.name for g in obj.groups.all()])
+    get_groups.short_description = 'Skupine'
+
+django_admin.site.unregister(User)
+django_admin.site.register(User, CustomUserAdmin)
+
 admin.site.register(Parcels_Owners, admin.ModelAdmin)
 admin.site.register(Parcels, ParcelsAdmin)
