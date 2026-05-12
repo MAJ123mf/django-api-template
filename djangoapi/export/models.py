6b41d3728f67a2e898fd2b8e0aff48ffadc4df2e
MajCapelnik <maj.capelnik@gmail.com>
2026-05-12 18:41:57 +0200

    Update models.py

--- djangoapi/export/models.py
+++ djangoapi/export/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ExportPermission(models.Model):
+    """Proxy model samo za namen definiranja custom pravice."""
+    class Meta:
+        managed = False          # Django ne ustvari tabele v bazi
+        default_permissions = () # odstranim default add/change/delete/view
+        permissions = [
+            ("can_export_geopackage", "Can export GeoPackage"),
+        ]
